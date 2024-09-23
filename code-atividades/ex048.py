c3cf8ca61030bcbe21af17bfb488e3e9dbe17ccb
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2024-09-23 15:41:53 +0000

    oraganizando exercícios

---
+++ code-atividades/ex048.py
@@ -0,0 +1,13 @@
+# Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+# Crie um laço de repetição que percorra a lista e imprima a string armazenada na variável a cada iteração.
+
+
+string = "Python"
+lista = ["Java", "C", "C++", "Python", "Ruby"]
+
+for i in lista:
+    print(string)
+    print(i)
+    print("")
+
